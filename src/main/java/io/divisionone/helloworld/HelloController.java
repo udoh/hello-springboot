fd10ddd78ef6d7b7f709e297a6d5c62608c7f7ee
Udo Hoppenworth <udo@autorentals.com>
2019-06-02 19:37:46 -0400

    initial commit

---
+++ src/main/java/io/divisionone/helloworld/HelloController.java
@@ -0,0 +1,14 @@
+package io.divisionone.helloworld;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static final Logger lo = LoggerFactory.getLogger(HelloController.class);
+
+
+}
