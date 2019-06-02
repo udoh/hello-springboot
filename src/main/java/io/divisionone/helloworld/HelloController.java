647459b5295a4094391a0b76849f38c778a5c2d9
Udo Hoppenworth <udo@autorentals.com>
2019-06-02 19:40:04 -0400

    Implement HelloController

--- src/main/java/io/divisionone/helloworld/HelloController.java
+++ src/main/java/io/divisionone/helloworld/HelloController.java
@@ -3,12 +3,18 @@ package io.divisionone.helloworld;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HelloController {
 
     private static final Logger lo = LoggerFactory.getLogger(HelloController.class);
 
-
+    @GetMapping("/api/v1/status")
+    public ResponseEntity<?> getStatus() {
+        return ResponseEntity.ok("Hello world!");
+    }
 }
